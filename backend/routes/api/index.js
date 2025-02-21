93ac59df1ff9fe77ae2d825b6e190aa190ca9ad0
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-02-20 23:52:47 -0800

    “Add - Files: backend/routes/api/index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,15 +8,40 @@ const { User } = require('../../db/models');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 
-router.use(restoreUser);
 
+router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
-// Restore user
-router.get('/restore-user', (req, res) => {
+
+// GET /api/restore-user
+router.get(
+  '/restore-user',
+  (req, res) => {
     return res.json(req.user);
-});
+  }
+);
+
+// test GET /api/require-auth
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// test GET /api/set-token-cookie
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-lition'
+//     }
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
+
 
 // test route
 // router.post('/test', function (req, res) {
