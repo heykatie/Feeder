0a7b0c3e9319e888d6e938753a2294998a6de629
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-02-21 16:05:21 -0800

    vite setup - Files: backend/routes/api/index.js frontend/.eslintrc.cjs frontend/src/App.jsx frontend/src/components/LoginFormModal/LoginFormModal.jsx frontend/src/components/LoginFormModal/index.js frontend/src/components/LoginFormPage/LoginFormPage.jsx frontend/src/components/LoginFormPage/index.js frontend/src/components/Navigation/Navigation.css frontend/src/components/Navigation/Navigation.jsx frontend/src/components/Navigation/OpenModalMenuItem.jsx frontend/src/components/Navigation/ProfileButton.jsx frontend/src/components/OpenModalButton/OpenModalButton.jsx frontend/src/components/OpenModalButton/index.js frontend/src/components/SignupFormModal/SignupFormModal.jsx frontend/src/components/SignupFormModal/index.jsx frontend/src/components/SignupFormPage/SignupFormPage.jsx frontend/src/components/SignupFormPage/index.js frontend/src/components/Splash/Splash.jsx frontend/src/components/Splash/index.js frontend/src/context/Modal.css frontend/src/context/Modal.jsx frontend/src/main.jsx frontend/src/router/Layout.jsx frontend/src/router/index.js frontend/src/store/csrf.js frontend/src/store/session.js frontend/src/store/store.js frontend/vite.config.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
+import { Provider as ReduxProvider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
+// import App from './App';
 import './index.css';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+import { router } from './router';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
-	window.store = store;
+if (import.meta.env.MODE !== 'production') {
+	restoreCSRF();
+
+	window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
 }
 
+// if (process.env.NODE_ENV !== 'production') {
+// 	window.store = store;
+// }
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ReduxProvider store={store}>
+			{/* <App /> */}
+			<RouterProvider router={router} />
+		</ReduxProvider>
+	</React.StrictMode>
 );
---
+++ frontend/src/store/session.js
@@ -0,0 +1,91 @@
+import { csrfFetch } from './csrf';
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+const setUser = (user) => {
+	return {
+		type: SET_USER,
+		payload: user,
+	};
+};
+
+const removeUser = () => {
+	return {
+		type: REMOVE_USER,
+	};
+};
+
+export const thunkAuthenticate = () => async (dispatch) => {
+	try {
+		const response = await csrfFetch('/api/restore-user');
+		if (response.ok) {
+			const data = await response.json();
+			dispatch(setUser(data));
+		}
+	} catch (e) {
+		return e;
+	}
+};
+
+export const login = (user) => async (dispatch) => {
+	const { credential, password } = user;
+	const response = await csrfFetch('/api/session', {
+		method: 'POST',
+		body: JSON.stringify({
+			credential,
+			password,
+		}),
+	});
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(setUser(data));
+    return response;
+  } else if (response.status < 500) {
+    const errorMessages = await response.json();
+    return errorMessages;
+  } else {
+    return { server: 'Something went wrong. Please try again' };
+  }
+};
+
+export const signup = (user) => async (dispatch) => {
+	const response = await csrfFetch('/api/users', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(user),
+	});
+
+	if (response.ok) {
+		const data = await response.json();
+		dispatch(setUser(data));
+	} else if (response.status < 500) {
+		const errorMessages = await response.json();
+		return errorMessages;
+	} else {
+		return { server: 'Something went wrong. Please try again' };
+	}
+};
+
+export const logout = () => async (dispatch) => {
+	await csrfFetch('/api/session', {
+		method: 'DELETE',
+	});
+	dispatch(removeUser());
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case SET_USER:
+			return { ...state, user: action.payload };
+		case REMOVE_USER:
+			return { ...state, user: null };
+		default:
+			return state;
+	}
+};
+
+export default sessionReducer;
