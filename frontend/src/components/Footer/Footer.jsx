743f674d8be58d22d7fe94a43d2fb8a3f1bdc94a
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-02-24 15:39:08 -0800

    footer links - Files: frontend/src/components/Footer/Footer.css frontend/src/components/Footer/Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,42 +1,47 @@
+import { useState } from 'react';
 import { FaEnvelope, FaGithub, FaLinkedin, FaInfoCircle } from 'react-icons/fa';
 import './Footer.css';
 
 const contactLinks = {
 	about: 'https://heykatie.github.io/',
 	email: 'mailto:katieleonght@gmail.com',
-	github: 'https://github.com/heykatie',
-	linkedin: 'https://www.linkedin.com/in/katieleonght/',
+	GitHub: 'https://github.com/heykatie',
+	LinkedIn: 'https://www.linkedin.com/in/katieleonght/',
 };
 
+const tooltips = {
+	about: 'heykatie.github.io',
+	email: 'katieleonght@gmail',
+	GitHub: '@heykatie',
+	LinkedIn: 'katieleonght',
+};
 
 const Footer = () => {
+	const [hovered, setHovered] = useState(null);
+
 	return (
 		<footer className='footer'>
 			<div className='footer-links'>
-				<a
-					href={contactLinks.about}
-					target='_blank'
-					rel='noopener noreferrer'
-					className='footer-link'>
-					<FaInfoCircle className='icon' /> About
-				</a>
-				<a href={contactLinks.email} className='footer-link'>
-					<FaEnvelope className='icon' /> Email
-				</a>
-				<a
-					href={contactLinks.github}
-					target='_blank'
-					rel='noopener noreferrer'
-					className='footer-link'>
-					<FaGithub className='icon' /> GitHub
-				</a>
-				<a
-					href={contactLinks.linkedin}
-					target='_blank'
-					rel='noopener noreferrer'
-					className='footer-link'>
-					<FaLinkedin className='icon' /> LinkedIn
-				</a>
+				{Object.entries(contactLinks).map(([key, link]) => (
+					<a
+						key={key}
+						href={link}
+						target={key !== 'email' ? '_blank' : undefined}
+						rel='noopener noreferrer'
+						className='footer-link'
+						onMouseEnter={() => setHovered(key)}
+						onMouseLeave={() => setHovered(null)}>
+						{key === 'about' && <FaInfoCircle className='icon' />}
+						{key === 'email' && <FaEnvelope className='icon' />}
+						{key === 'GitHub' && <FaGithub className='icon' />}
+						{key === 'LinkedIn' && <FaLinkedin className='icon' />}
+						<span className='link-text'>
+							{hovered === key
+								? tooltips[key]
+								: key.charAt(0).toUpperCase() + key.slice(1)}
+						</span>
+					</a>
+				))}
 			</div>
 		</footer>
 	);
