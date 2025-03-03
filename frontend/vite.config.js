97c45e004011048baec39b4f60e68deb4f3d35bd
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-03-02 21:56:34 -0800

    build & deploy - Files: frontend/src/components/Recipes/Recipes.css frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig((mode) => ({
 	plugins: [
 		react(),
@@ -18,6 +17,21 @@ export default defineConfig((mode) => ({
 				target: 'http://localhost:8080',
 				changeOrigin: true,
 				secure: true,
+			},
+		},
+	},
+	build: {
+		// chunkSizeWarningLimit: 700,
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						if (id.includes('react')) return 'react-vendor';
+						if (id.includes('lodash')) return 'lodash-vendor';
+						if (id.includes('axios')) return 'axios-vendor';
+						return 'vendor';
+					}
+				},
 			},
 		},
 	},
