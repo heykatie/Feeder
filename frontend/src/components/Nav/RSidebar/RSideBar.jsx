a5770ca1de0d225594aee8e66bfbd4a3b0429daf
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-03-02 22:06:41 -0800

    build & deploy - Files: frontend/src/components/Nav/RSidebar/RSideBar.jsx

--- frontend/src/components/Nav/RSidebar/RSideBar.jsx
+++ frontend/src/components/Nav/RSidebar/RSideBar.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { logout } from '../../../redux/session';
-import './RSidebar.css';
+import './RSideBar.css';
 
 const RSidebar = ({ showSidebar, toggleSidebar }) => {
 	const dispatch = useDispatch();
